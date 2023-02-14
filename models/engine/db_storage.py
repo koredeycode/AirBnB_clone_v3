0c6c25420777e066bec294c04e83f4d32d851a13
koredeycode <koredey4u@gmail.com>
2023-02-14 09:46:25 +0000

    created the storage_get_count branch

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,4 +73,15 @@ class DBStorage:
 
     def close(self):
         """call remove() method on the private session attribute"""
-        self.__session.remove()
+
+    def get(self, cls, id):
+        "retrieve one object"
+        objects = self.all(cls)
+        for key, obj in objects.items():
+            if id == key.split(".")[-1]:
+                return (obj)
+        return (none)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return (len(self.all(cls)))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -68,3 +68,15 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        "retrieve one object"
+        objects = self.all(cls)
+        for key, obj in objects.items():
+            if id == key.split(".")[-1]:
+                return (obj)
+        return (none)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return (len(self.all(cls)))
