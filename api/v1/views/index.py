cc030b58fff59c45d9a120618be6361ee6bca83b
koredeycode <koredey4u@gmail.com>
2023-02-14 20:43:29 +0000

    push

---
+++ api/v1/views/index.py
@@ -0,0 +1,30 @@
+from api.v1.views import app_views
+from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.reviews import Review
+from models.states import State
+from models.user import User
+from models import storage
+
+
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """return the status of the server"""
+    return (jsonify({"status": "OK"}))
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def object_count():
+    """retrieves the number of each objects by type"""
+    classes = {
+                "amenities": Amenity, "cities": City, 
+                "places": Place, "reviews": Review,
+                "states": State, "users": User
+                }
+
+    objects = {}
+    for name, cls in classes.items():
+        objects[name] = storage.count(cls)
+
+    return (jsonify(objects))
