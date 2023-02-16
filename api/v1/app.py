d1f438b5feaf43cd4d065efd2ba416a13bfaca28
koredeycode <koredey4u@gmail.com>
2023-02-16 11:11:51 +0000

    module documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""The app module. It runs the application"""
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""the index for the app view"""
 from api.v1.views import app_views
 from flask import jsonify
 from models.amenity import Amenity
